c08da69285ec090eb27ebc0a82ebd143766e22e3
John <John01166@ya.ru>
2022-10-17 18:28:25 +0300

    DZ19

--- 19/Program.cs
+++ 19/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и
+//  проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+System.Console.WriteLine("Введите пятизначное число");
+int num = Convert.ToInt32(System.Console.ReadLine());
+// System.Console.WriteLine(num/1000);
+// System.Console.WriteLine(num%100);
