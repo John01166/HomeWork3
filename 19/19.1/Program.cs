bc8d6b756071cc7d5cebfae3f6a8671bc65293aa
John <John01166@ya.ru>
2022-10-18 19:40:52 +0300

    DZ19.1

--- 19/19.1/Program.cs
+++ 19/19.1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string Polindrome()
+{
+    System.Console.Write("Введите число:");
+    string number = System.Console.ReadLine();
+    string result = $"Число {number} является полиндромом";
+
+    int[] array = new int[number.Length];
+    for (int i = 0; i<number.Length;i++)
+    {
+
+        array[i] = Convert.ToInt32(number.Substring(i,1));
+    }
+
+    int [] revers = new int [array.Length];
+    for (int i = 0; i < revers.Length; i++)
+    {
+        revers[i] = array[array.Length -1 - i];
+    }
+
+    bool find = false;
+    for (int i=0; i<array.Length; i++)
+        if (array[i] != revers[i])
+        {
+            find = true;
+            break;
+        }
+    
+    if (find)
+        result = $"Число {number} не является полиндромом";
+    return result;
+}
