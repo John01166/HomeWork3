eab255f2396dccfdaeaacc19f32333c2b53b8692
John <John01166@ya.ru>
2022-10-18 17:45:58 +0300

    DZ19 string

--- 19/Program.cs
+++ 19/Program.cs
@@ -4,7 +4,25 @@
 // 14212 -> нет
 // 12821 -> да
 // 23432 -> да
-System.Console.WriteLine("Введите пятизначное число");
-int num = Convert.ToInt32(System.Console.ReadLine());
-// System.Console.WriteLine(num/1000);
-// System.Console.WriteLine(num%100);
+System.Console.WriteLine("Введите пятизнач число");
+Console.ResetColor(); // возврат к белому цвету
+
+string number = Console.ReadLine(); //Вводим число в строку, чтобы проверить его длину
+int dlina = number.Length;          // Проверяем длину
+if (dlina == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        System.Console.WriteLine($"{number} - Палиндром");
+    }
+    else 
+    {
+     System.Console.WriteLine($"{number} - не палиндром");
+    }
+}
+else 
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    System.Console.WriteLine($"Attention: {number} - некорректно введено число"); // ошибка
+    Console.ResetColor();
+}
