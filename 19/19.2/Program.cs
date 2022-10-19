39eeeef6f405cff4f3d8318d260c3ba7427a5dad
John <John01166@ya.ru>
2022-10-19 17:36:19 +0300

    DZ19.2

--- 19/19.2/Program.cs
+++ 19/19.2/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+// int  x = System.Console.ReadLine();
+
+// for (x=0; x>0; x++)
+// {
+//     num[i] = x%10;
+//     x = x/10;
+// }
+
+// System.Console.WriteLine(num);
+
+int result = 0;
+int  x = Convert.ToInt32(System.Console.ReadLine());
+int x1 = x;
+while (x1 >0)
+{
+    result = result *10 +x1%10;
+    x1/=10;
+}
+System.Console.WriteLine(result);
+if (result == x)  
+    System.Console.WriteLine($"Число {result} является зеркальным");
+else 
+    System.Console.WriteLine($"Число {result} не является зеркальным");
